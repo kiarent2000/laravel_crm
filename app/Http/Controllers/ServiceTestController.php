2ebbad7c52ba333850757b42c1416c06dde78a20
Sergiy Soroka <vesper7777777@gmai.com>
2022-12-06 11:41:33 +0200

    3545

--- app/Http/Controllers/ServiceTestController.php
+++ app/Http/Controllers/ServiceTestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\PriceCalculateService;
 
 class ServiceTestController extends Controller
 {
--- app/Providers/PriceServiceProvider.php
+++ app/Providers/PriceServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\PriceCalculateService;
+
 
 class PriceServiceProvider extends ServiceProvider
 {
